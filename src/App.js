1e2e918d76e38c3c47f2a85a890e6d0b715d991b
soham117 <purohit.soham.ps@gmail.com>
2022-01-22 23:26:54 +0530

    Added code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Navbar1 from "./Components/Navbar1";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Twitter Jobs</h1>
+      <Navbar1 />
+    </div>
+  );
+}
